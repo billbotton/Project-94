84bcc06a8c51616ed7d63ea94e12ff39a7c7e2e6
Bill Bottom <92114190+billbotton@users.noreply.github.com>
2022-06-24 15:46:45 -0500

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAuJ2ZVP9iN6GrZnAvdtVE_iW_LJCFJxx8",
+    authDomain: "let-s-chat-aa4b4.firebaseapp.com",
+    projectId: "let-s-chat-aa4b4",
+    storageBucket: "let-s-chat-aa4b4.appspot.com",
+    messagingSenderId: "293069127668",
+    appId: "1:293069127668:web:815409ad2d3ebc63e383a1",
+    measurementId: "G-N56L1EFKJ1"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
